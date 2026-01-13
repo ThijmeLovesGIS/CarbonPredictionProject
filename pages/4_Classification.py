25272759b8ecc208b40d9afc1d03127a7d04e98e
Thijme <th.jonckers@gmail.com>
2026-01-13 10:51:22 +0200

    Refactor classification page for clarity and updates

--- pages/4_Classification.py
+++ pages/4_Classification.py
@@ -144,11 +144,18 @@ classification = leafmap.Map(
 )
 classification.add_basemap("SATELLITE")
 clas_file = "Data/Forest_classification.tif"
+#classification.add_raster(
+            #clas_file,
+            #layer_name="Forest classification",
+            #palette=["#00000000", "#006400", "#90ee90"],
+           # nodata=0
+#)
 classification.add_raster(
-            clas_file,
-            layer_name="Forest classification",
-            palette=["#00000000", "#006400", "#90ee90"],
-            nodata=0
+    clas_file,
+    layer_name="Forest classification",
+    values=[1, 2],
+    palette=["#006400", "#90ee90"],
+    nodata=0
 )
 legend_dict = {
     "Coniferous forest": "#006400",
@@ -168,6 +175,7 @@ st.page_link(
     "pages/5_Total_carbon_stored.py",
     label="-> Carbon prediction"
 )
+
 
 
 

f876b00aa32ac598447d0507bfdad04db235a92a
Thijme <th.jonckers@gmail.com>
2026-01-15 10:31:02 +0200

    Update 3_Carbon_calculations.py

--- pages/3_Carbon_calculations.py
+++ pages/3_Carbon_calculations.py
@@ -2,14 +2,14 @@ import streamlit as st
 import pandas as pd
 
 st.title("Carbon calculations")
-st.write("To be able to calculate the amount of carbon stored in the trees various calculations needed to be applied to the measurements.")
+st.write("To be able to calculate the amount of carbon stored in the trees, various calculations needed to be applied.")
 st.space(size="small")
-st.header("Formula's")
+st.header("Formulas")
 st.write("""
-        The measurements that were taken were the circumference at breast height in meters which were converted earlier to diameter at breast height in centimeters and the height in meters. These two variables are needed to be able to estimate the amount of carbon stored in the trees in kg. For these various formula's were used based on the following research:
+        The measurements that were taken were the circumference at breast height in meters which were converted earlier to diameter at breast height in centimeters and the height in meters. These two variables are needed to be able to estimate the amount of carbon stored in the trees in kg. For these various formulas were used based on the following research:
         - For most of the formulas except the above ground biomass this website was used: 
             - https://www.ecomatcher.com/how-to-calculate-co2-sequestration/
-        - For the above ground biomass this reasearch paper was used. The Chave's formula was mainly made for tropical forests, but can also be used for mediterranean forests:
+        - For the above ground biomass this research paper was used. The Chave's formula was mainly made for tropical forests, but can also be used for mediterranean forests:
             - https://www.researchgate.net/publication/262197290_Improved_allometric_models_to_estimate_the_aboveground_biomass_of_tropical_trees
         - To be able to get the right data for the above ground biomass the wood density of species needed to be found for this the following database was used:
             - https://www.fs.usda.gov/nrs/pubs/gtr/gtr-nrs200-2023_appendixes/gtr_nrs200-2023_appendix11.pdf
@@ -17,7 +17,7 @@ st.write("""
 
 st.space(size="small")
 
-st.subheader("Coeffiecients")
+st.subheader("Coefficients")
 st.write("During the field work some species could be identified based on these the mean wood density per forest type could be calculated following the database mentioned before:")
 wood_density_species = pd.DataFrame({
     "Species": ["Olea europaea", "Quercus ilex", "Pinus nigra", "Pinus brutia"],
@@ -30,7 +30,7 @@ st.write("""
          - Broadleaf: 0.79 g/cm3
          - Coniferous: 0.43 g/cm3
          """)
-st.write("If the research would be conducted and the species would be unknown in a mediterranean forest the wood density value of 0.6 g/cm3 can be used. (https://pmc.ncbi.nlm.nih.gov/articles/PMC11618071/)")
+st.write("If further research would be conducted on this subject and the species would be unknown in a mediterranean forest, the wood density value of 0.6 g/cm3 can be used. (https://pmc.ncbi.nlm.nih.gov/articles/PMC11618071/)")
 
 st.space(size="small")
 
@@ -80,3 +80,4 @@ st.page_link(
     label="-> Classification"
 
 )
+
